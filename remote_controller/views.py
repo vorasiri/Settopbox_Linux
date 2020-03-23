b4e0074542c1cfec7d58e3b27568e0027ddb2a4a
Kongpob <kongpop.rak@hotmail.co.th>
2020-03-23 16:14:23 +0700

    add get method to page for control the mode

--- remote_controller/views.py
+++ remote_controller/views.py
@@ -3,7 +3,17 @@ from remote_controller.models import StateHolder
 
 # Create your views here.
 def home(request):
+    state = StateHolder.objects.all()[0]
     if request.method == "POST":
         pass
-    
+    else:
+        if request.GET.get('pills'):
+            mode = request.GET.get('pills')
+            if mode == 'full':
+                state.controlDegree = 2
+            elif mode == 'semi':
+                state.controlDegree = 1
+            else:
+                state.controlDegree = 0
+            state.save()
     return render(request, 'home.html', {'StateHolder': StateHolder.objects.all()[0]})

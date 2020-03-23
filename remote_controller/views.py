3ce64257054dfb700fd7e026040f128802bdd603
Kongpob <kongpop.rak@hotmail.co.th>
2020-03-23 16:35:00 +0700

    add switch control panel able to edit DB

--- remote_controller/views.py
+++ remote_controller/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect
 from remote_controller.models import StateHolder
 
 # Create your views here.
 def home(request):
     state = StateHolder.objects.all()[0]
     if request.method == "POST":
-        pass
+        state.mediaSharing = 0
+        state.homeEntertain = 0
+        state.peer2peer = 0
+        if request.POST.get('switchMedia'):
+            state.mediaSharing = 1
+        if request.POST.get('switchHomeEnt'):
+            state.homeEntertain = 1
+        if request.POST.get('switchP2P'):
+            state.peer2peer = 1
+        state.save()
+        return HttpResponseRedirect('/')
     else:
         if request.GET.get('pills'):
             mode = request.GET.get('pills')
@@ -15,5 +25,6 @@ def home(request):
                 state.controlDegree = 1
             else:
                 state.controlDegree = 0
-            state.save()
+
+    state.save()
     return render(request, 'home.html', {'StateHolder': StateHolder.objects.all()[0]})
